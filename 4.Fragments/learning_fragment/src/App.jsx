4688a39e6154b4b86797bbc4627dedc39846344b
demo <shubhamkokate5554@gmail.com>
2024-10-28 21:25:34 +0530

    conditional rendering

--- 4.Fragments/learning_fragment/src/App.jsx
+++ 4.Fragments/learning_fragment/src/App.jsx
@@ -2,11 +2,16 @@
 import "./App.css";
 
 function App() {
-  let fooditem=['Dal','Milk','Roti','Green Vegetable','salad','Ghee']
+  // let fooditem=['Dal','Milk','Roti','Green Vegetable','salad','Ghee']
+  let fooditem=[]
+
   return (
     // <React.Fragment>
     <>
       <h1>Healthy Food</h1>
+      {fooditem.length===0 ? <h3>i am still hungry.</h3> : null }    
+      {fooditem===0 && <h3>i am still hungry.</h3> }
+
       <ul class="list-group">
         {fooditem.map((item)=>(<li key={item} class="list-group-item">{item}</li> ))}
       </ul>
