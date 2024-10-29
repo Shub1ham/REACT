8f99e81ec18a897e51b5ff2ae83bf6c40f95c158
demo <shubhamkokate5554@gmail.com>
2024-10-30 02:32:20 +0530

    passing children

--- 4.Fragments/learning_fragment/src/App.jsx
+++ 4.Fragments/learning_fragment/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Heading from "./component/heading";
 import List from "./component/list";
 import Error from "./component/error_message";
+import Container from "./component/Container";
 function App() {
   let fooditem=['Dal','Milk','Roti','Green Vegetable','salad','Ghee']
   // let fooditem = [];
@@ -10,9 +11,14 @@ function App() {
   return (
     // <React.Fragment>
     <>
+    <Container>
       <Heading />
       <Error items={fooditem}></Error>
       <List items={fooditem}></List>
+    </Container>
+    <Container>
+      <p>above is a list of healthy food</p>
+    </Container>
     </>
     /* </React.Fragment> */
   );
