71d28759d313fc6a5598c9e520af2851a7ac6885
Shub1ham <shubhamkokate5554@gmail.com>
2024-11-08 18:29:11 +0530

    dynamic component

--- 1-create-first-component/src/App.jsx
+++ 1-create-first-component/src/App.jsx
@@ -1,10 +1,12 @@
 import Button from "./Button";
+import Hello from "./hello";
+
 
 function App() {
   return (
     <div>
       <h1>hello world</h1>
-      <Button></Button>
+      <Hello></Hello>
     </div>
   );
 }
---
+++ 1-create-first-component/src/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello(){
+    let name='shubham'
+    let fullname=()=>{
+        return 'shubham kokate'
+    }
+    return <h1>hello my name is {fullname()}</h1>
+}
+
+export default Hello
