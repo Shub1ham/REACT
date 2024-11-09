9379f0f0735d2dead477fc62755d7141730b7489
Shub1ham <shubhamkokate5554@gmail.com>
2024-11-09 17:23:22 +0530

    fragments

---
+++ 3-fragments/learning-fragment/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Green Vegetable</li>
+        <li class="list-group-item">Roti</li>
+        <li class="list-group-item">Salad</li>
+        <li class="list-group-item">Milk</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
