64786664811c66d73d44fe7470bd640daecf7265
Shub1ham <shubhamkokate5554@gmail.com>
2024-11-09 17:34:00 +0530

    map method

--- 3-fragments/learning-fragment/src/App.jsx
+++ 3-fragments/learning-fragment/src/App.jsx
@@ -1,13 +1,11 @@
 function App() {
+  let fooditems=["Dal","Green Vegetable","Roti","Salad","Milk","Ghee"]
   return (
     <>
       <h1>Healthy Food</h1>
-      <ul class="list-group">
-        <li class="list-group-item">Dal</li>
-        <li class="list-group-item">Green Vegetable</li>
-        <li class="list-group-item">Roti</li>
-        <li class="list-group-item">Salad</li>
-        <li class="list-group-item">Milk</li>
+      <ul className="list-group">
+        {fooditems.map(item=>(<li key={item} class="list-group-item">{item}</li>))}
+        
       </ul>
     </>
   );
