1f05abc06d5f9c98c5d85b0e7d7f25aa07b38662
Shub1ham <shubhamkokate5554@gmail.com>
2024-11-10 14:40:44 +0530

    css module

--- 3-fragments/learning-fragment/src/App.jsx
+++ 3-fragments/learning-fragment/src/App.jsx
@@ -1,12 +1,14 @@
 import Fooditems from "./component/fooditems";
 import Errormessage from "./component/ErrorMessage";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css";
 
 function App() {
   let fooditems=["Dal","Green Vegetable","Roti","Salad","Milk","Ghee"]
   
   return (
     <center>
-      <h1>Healthy Food</h1>
+      <h1 className="heading">Healthy Food</h1>
       <Errormessage items={fooditems}></Errormessage>              
       <Fooditems items={fooditems}></Fooditems>
     </center>
--- 3-fragments/learning-fragment/src/component/Item.jsx
+++ 3-fragments/learning-fragment/src/component/Item.jsx
@@ -1,7 +1,8 @@
+import styles from "./Item.module.css";
 const Item = ({foodItem}) => {
   return (
-    <li className="list-group-item">
-      {foodItem}
+    <li className={`${styles["sk-item"]} list-group-item`}>
+      <span className={styles["sk-span"]}>{foodItem}</span>
     </li>
   );
 };
