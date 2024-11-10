76ff9e0deb927f75f7a75c05007130ed25180e3e
Shub1ham <shubhamkokate5554@gmail.com>
2024-11-10 12:55:12 +0530

    conditional rendering

--- 3-fragments/learning-fragment/src/App.jsx
+++ 3-fragments/learning-fragment/src/App.jsx
@@ -1,13 +1,16 @@
 function App() {
-  let fooditems=["Dal","Green Vegetable","Roti","Salad","Milk","Ghee"]
+  // let fooditems=["Dal","Green Vegetable","Roti","Salad","Milk","Ghee"]
+  let fooditems=[]
   return (
-    <>
+    <center>
       <h1>Healthy Food</h1>
+      {/* {fooditems.length===0 ? <h3>i am still hungry</h3> : null} */}
+      {fooditems.length===0 && <h3>i am still hungry</h3>}           
       <ul className="list-group">
         {fooditems.map(item=>(<li key={item} class="list-group-item">{item}</li>))}
         
       </ul>
-    </>
+    </center>
   );
 }
 
